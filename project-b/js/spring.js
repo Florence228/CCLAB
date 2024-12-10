bcfb574b29003c5beaa2eea673331c1cef65dd62
Florence228 <kj2527@nyu.edu>
2024-12-10 11:04:00 +0800

    kk

--- project-b/js/spring.js
+++ project-b/js/spring.js
@@ -13,7 +13,7 @@ function preload() {
   img3 = loadImage('assets/summer-phase3.jpg');
   img4 = loadImage('assets/summer-phase4.jpg');
   img5 = loadImage('assets/summer-phase5.jpg');
-  img6 = loadImage('assets/summer-phase6.jpg');
+  img6 = loadImage('assets/spring-phase6.png');
   img7 = loadImage('assets/tree1.jpeg');
   img8 = loadImage('assets/water.jpg');
   img9 = loadImage('assets/sunshine.jpg');
